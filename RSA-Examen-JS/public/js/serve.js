f234f6f4d877864995e72d6156d7d89408341f9b
Luis Fernando Tenorio Aspiros <taspiluis16@gmail.com>
2020-11-26 23:55:33 -0600

    correcion server

--- RSA-Examen-JS/public/js/serve.js
+++ RSA-Examen-JS/public/js/serve.js
@@ -4,16 +4,26 @@ const input = document.getElementById("inputs");
 
 
 socket.on('Conectado', (data) => {
-    console.log("Nuevo usuario conectado");
+    if (input.children.length > 4) {
+        input.children[0].remove();
+    }
+    //console.log("Nuevo usuario conectado");
     input.innerHTML += '<div class="alert alert-light">'+
     '<strong>Nuevo Usuario conectado ID socket</strong>: '+data+
     '</div>';
 });
 
 socket.on('msg-enc', (data) => {
-    console.log("Mensaje incriptado: ", data.msg);
+    if (input.children.length > 4) {
+        input.children[0].remove();
+        input.children[1].remove();
+    }
+    let tex = data.msg.split("");
+    const regex = /[,]/gi;
+    tex = tex.toString().replace(regex, ' ');
+    //console.log("Mensaje incriptado: ", tex);
     input.innerHTML += '<div class="alert alert-info">'+
-    '<strong>Mensaje incriptado de '+data.user+'</strong>: '+data.msg+
+    '<strong>Mensaje incriptado de '+data.user+'</strong>: '+tex+
     '</div>';
     input.innerHTML += '<div class="alert alert-danger">'+
     '<strong>Mensaje desincriptado de '+data.user+'</strong>: '+data.msgd+
